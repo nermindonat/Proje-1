95770e2b94f33dfde543dcc0570b8487f1f8bb38
hikdogru <hikdogru@gmail.com>
2020-08-29 16:11:24 +0300

    Katmanlar oluşturuldu.

---
+++ GorevYonetimSistemi.EntitySiniflar/AnaBilimDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorevYonetimSistemi.EntitySiniflar
+{
+    public class AnaBilimDal
+    {
+        public int AbdId { get; set; }
+        public string AbdAd { get; set; }
+    }
+}
---
+++ GorevYonetimSistemi.VeriKatmani/EntityContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorevYonetimSistemi.VeriKatmani
+{
+    public class EntityContext:DbContext
+    {
+
+    }
+}
